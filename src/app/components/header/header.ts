56d5a3b33528acb5bd6bd74307aa4eb9ec46c3c0
SecretUser tttfy <chotk@Setup.com>
2026-02-23 20:36:05 +0200

    header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar'
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './headerComp.html',
-  imports:[MatToolbarModule]
+  styleUrl: './header.css',
+  standalone: true,
+  encapsulation: ViewEncapsulation.None,//encapsulation: off, styles/subelements: can be assigned globally 
+  imports:[RouterModule]
 })
 export class Header {
-
+  currentLang='English';
+  
+  setLang(lang: string){
+    this.currentLang= lang;
+  }
 }

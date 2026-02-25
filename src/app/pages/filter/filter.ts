e44370e065352d05788c85a2ab36045b0cf87b22
SecretUser tttfy <chotk@Setup.com>
2026-02-25 15:54:36 +0200

    filter page

--- src/app/pages/filter/filter.ts
+++ src/app/pages/filter/filter.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { Header } from '../../components/header/header';
+import { Search } from '../../components/search/search';
 
 @Component({
   selector: 'app-filter',
   templateUrl:'./filterCOmp.html',
-  styleUrl:'./filter.css',
+  styleUrls:['./filter.css', '../../components/search/search.css'],
   standalone:true,
-  imports: []
+  encapsulation:ViewEncapsulation.None,
+  imports: [Header, Search, TranslateModule, RouterModule]
 })
 export class Filter {
 
